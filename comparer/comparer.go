b966dd137930586a6f945d36de7a1ee2c4c2b5ef
irenicaa <root@irenica.pro>
2020-09-19 18:31:52 +0300

    Add the SourceDiff structure

--- comparer/comparer.go
+++ comparer/comparer.go
@@ -1,8 +1,18 @@
 package comparer
 
+import "github.com/irenicaa/repos-checker/models"
+
 // RepoDiff ...
 type RepoDiff struct {
 	Name              string
 	LastCommitInLeft  string
 	LastCommitInRight string
+}
+
+// SourceDiff ...
+type SourceDiff struct {
+	Equal         []models.RepoState
+	Diff          []RepoDiff
+	MissedInLeft  []models.RepoState
+	MissedInRight []models.RepoState
 }
