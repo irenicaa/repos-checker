f042be27dc1b99ec962d1196e05c6d625b70b507
irenicaa <root@irenica.pro>
2020-09-19 19:21:11 +0300

    Add the CompareRepos function

--- comparer/comparer.go
+++ comparer/comparer.go
@@ -16,3 +16,37 @@ type SourceDiff struct {
 	MissedInLeft  []models.RepoState
 	MissedInRight []models.RepoState
 }
+
+// CompareRepos ...
+func CompareRepos(
+	left models.SourceState,
+	right models.SourceState,
+) SourceDiff {
+	sourceDiff := SourceDiff{}
+	for _, itemLeft := range left.Repos {
+		for _, itemRight := range right.Repos {
+			if itemLeft.Name == itemRight.Name {
+				if itemLeft.LastCommit == itemRight.LastCommit {
+					sourceDiff.Equal = append(sourceDiff.Equal, itemLeft)
+				} else {
+					sourceDiff.Diff = append(sourceDiff.Diff, RepoDiff{
+						Name:              itemLeft.Name,
+						LastCommitInLeft:  itemLeft.LastCommit,
+						LastCommitInRight: itemRight.LastCommit,
+					})
+				}
+			} else {
+				sourceDiff.MissedInRight = append(sourceDiff.MissedInRight, itemLeft)
+			}
+		}
+	}
+	for _, itemRight := range right.Repos {
+		for _, itemLeft := range left.Repos {
+			if itemRight.Name != itemLeft.Name {
+				sourceDiff.MissedInLeft = append(sourceDiff.MissedInRight, itemRight)
+			}
+		}
+	}
+
+	return sourceDiff
+}
