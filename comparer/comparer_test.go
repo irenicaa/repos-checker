97282754ba24bab0fd584aa70394e0ee7da591d6
irenicaa <root@irenica.pro>
2020-10-02 22:43:10 +0300

    Add tests for the CompareSources function

--- comparer/comparer_test.go
+++ comparer/comparer_test.go
@@ -7,6 +7,93 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestCompareSources(t *testing.T) {
+	type args struct {
+		sources   []models.SourceState
+		reference models.SourceState
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want []SourceDiff
+	}{
+		{
+			name: "empty",
+			args: args{
+				sources: []models.SourceState{},
+				reference: models.SourceState{
+					Name: "reference",
+					Repos: []models.RepoState{
+						{Name: "one", LastCommit: "100"},
+						{Name: "two", LastCommit: "200"},
+					},
+				},
+			},
+			want: []SourceDiff{},
+		},
+		{
+			name: "nonempty",
+			args: args{
+				sources: []models.SourceState{
+					{
+						Name: "source-one",
+						Repos: []models.RepoState{
+							{Name: "one", LastCommit: "100"},
+							{Name: "two", LastCommit: "250"},
+						},
+					},
+					{
+						Name: "source-two",
+						Repos: []models.RepoState{
+							{Name: "three", LastCommit: "300"},
+							{Name: "four", LastCommit: "400"},
+						},
+					},
+				},
+				reference: models.SourceState{
+					Name: "reference",
+					Repos: []models.RepoState{
+						{Name: "one", LastCommit: "100"},
+						{Name: "two", LastCommit: "200"},
+					},
+				},
+			},
+			want: []SourceDiff{
+				{
+					NameOfLeft:  "source-one",
+					NameOfRight: "reference",
+					Equal: []models.RepoState{
+						{Name: "one", LastCommit: "100"},
+					},
+					Diff: []RepoDiff{
+						{Name: "two", LastCommitInLeft: "250", LastCommitInRight: "200"},
+					},
+				},
+				{
+					NameOfLeft:  "source-two",
+					NameOfRight: "reference",
+					MissedInLeft: []models.RepoState{
+						{Name: "one", LastCommit: "100"},
+						{Name: "two", LastCommit: "200"},
+					},
+					MissedInRight: []models.RepoState{
+						{Name: "three", LastCommit: "300"},
+						{Name: "four", LastCommit: "400"},
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := CompareSources(tt.args.sources, tt.args.reference)
+
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
 func TestCompareRepos(t *testing.T) {
 	type args struct {
 		left  models.SourceState
