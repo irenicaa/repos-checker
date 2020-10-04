0bfa14541d5186c7ef129e381f5b677dbb532989
irenicaa <root@irenica.pro>
2020-10-04 21:54:10 +0300

    Fix the models package

--- models/models.go
+++ models/models.go
@@ -1,13 +1,13 @@
 package models
 
-// RepoState ...
-type RepoState struct {
-	Name       string
-	LastCommit string
-}
-
 // SourceState ...
 type SourceState struct {
 	Name  string
 	Repos []RepoState
+}
+
+// RepoState ...
+type RepoState struct {
+	Name       string
+	LastCommit string
 }
