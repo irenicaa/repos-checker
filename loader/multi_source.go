f06125b78e63999af531f3cca395d5cd3c691481
irenicaa <root@irenica.pro>
2020-10-18 18:07:38 +0300

    Add the MultiSource.Name method

--- loader/multi_source.go
+++ loader/multi_source.go
@@ -1,4 +1,17 @@
 package loader
 
+import "strings"
+
 // MultiSource ...
 type MultiSource []Source
+
+// Name ...
+func (sources MultiSource) Name() string {
+	names := []string{}
+	for _, source := range sources {
+		name := source.Name()
+		names = append(names, name)
+	}
+
+	return strings.Join(names, "|")
+}
