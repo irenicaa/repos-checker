766cde39d199c4bf41b671cb75efaa7c5d90ee6b
irenicaa <root@irenica.pro>
2020-10-28 02:28:35 +0300

    Add the github.Source.LoadRepos method

--- loader/sources/github/source.go
+++ loader/sources/github/source.go
@@ -1,9 +1,34 @@
 package github
 
+import "github.com/irenicaa/repos-checker/models"
+
 // Source ...
-type Source struct{}
+type Source struct {
+	Owner string
+}
 
 // Name ...
 func (source Source) Name() string {
 	return "github"
+}
+
+// LoadRepos ...
+func (source Source) LoadRepos() ([]models.RepoState, error) {
+	const maxPageSize = 100
+	repos, err := GetRepos(source.Owner, maxPageSize)
+	if err != nil {
+		return nil, err
+	}
+
+	var reposStates []models.RepoState
+	for _, repo := range repos {
+		repoState, err := GetLastCommit(source.Owner, repo)
+		if err != nil {
+			return nil, err
+		}
+
+		reposStates = append(reposStates, repoState)
+	}
+
+	return reposStates, nil
 }
