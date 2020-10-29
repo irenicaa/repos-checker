46c5cd9e13024b27b13c0e03c8614ee9d4afe065
irenicaa <root@irenica.pro>
2020-10-29 19:41:18 +0300

    Check the bitbucket.SendRequest function

--- cmd/sources-checker/main.go
+++ cmd/sources-checker/main.go
@@ -5,11 +5,18 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/irenicaa/repos-checker/loader/sources/bitbucket"
 	"github.com/irenicaa/repos-checker/loader/sources/github"
 )
 
+type workspace struct {
+	Name string
+	Slug string
+	Type string
+}
+
 func main() {
-	source := flag.String("source", "github", "")
+	source := flag.String("source", "bitbucket", "")
 	flag.Parse()
 
 	switch *source {
@@ -21,6 +28,13 @@ func main() {
 		}
 
 		fmt.Printf("%s %+v\n", source.Name(), reposStates)
+	case "bitbucket":
+		var workspace workspace // nolint: vetshadow
+		if err := bitbucket.SendRequest("/workspaces/MartinFelis", nil, &workspace); err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("%+v\n", workspace)
 	default:
 		log.Fatal("unknown source")
 	}
