4dc08f32ebda8a4065a5ed13e58cf68a832e2250
irenicaa <root@irenica.pro>
2020-10-30 21:44:47 +0300

    Check the bitbucket.GetLastCommit function

--- cmd/sources-checker/main.go
+++ cmd/sources-checker/main.go
@@ -9,12 +9,6 @@ import (
 	"github.com/irenicaa/repos-checker/loader/sources/github"
 )
 
-type workspace struct {
-	Name string
-	Slug string
-	Type string
-}
-
 func main() {
 	source := flag.String("source", "bitbucket", "")
 	flag.Parse()
@@ -29,12 +23,12 @@ func main() {
 
 		fmt.Printf("%s %+v\n", source.Name(), reposStates)
 	case "bitbucket":
-		var workspace workspace // nolint: vetshadow
-		if err := bitbucket.SendRequest("/workspaces/MartinFelis", nil, &workspace); err != nil {
+		repoState, err := bitbucket.GetLastCommit("MartinFelis", "love-android-sdl2")
+		if err != nil {
 			log.Fatal(err)
 		}
 
-		fmt.Printf("%+v\n", workspace)
+		fmt.Printf("%+v\n", repoState)
 	default:
 		log.Fatal("unknown source")
 	}
