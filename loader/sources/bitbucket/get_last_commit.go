3195bf26b7269f41c75fe0dc2c086e2dc774e7ce
irenicaa <root@irenica.pro>
2020-10-30 21:44:14 +0300

    Add the bitbucket.GetLastCommit function

---
+++ loader/sources/bitbucket/get_last_commit.go
@@ -0,0 +1,35 @@
+package bitbucket
+
+import (
+	"errors"
+	"fmt"
+	"net/url"
+
+	"github.com/irenicaa/repos-checker/models"
+)
+
+type commitsPage struct {
+	Values []commit
+}
+
+type commit struct {
+	SHA string `json:"hash"`
+}
+
+// GetLastCommit ...
+func GetLastCommit(workspace string, repo string) (models.RepoState, error) {
+	parameters := url.Values{}
+	parameters.Add("pagelen", "1")
+
+	var commits commitsPage
+	endpoint := fmt.Sprintf("/repositories/%s/%s/commits", workspace, repo)
+	if err := SendRequest(endpoint, parameters, &commits); err != nil {
+		return models.RepoState{}, err
+	}
+	if len(commits.Values) == 0 {
+		return models.RepoState{}, errors.New("no commits")
+	}
+
+	repoState := models.RepoState{Name: repo, LastCommit: commits.Values[0].SHA}
+	return repoState, nil
+}
