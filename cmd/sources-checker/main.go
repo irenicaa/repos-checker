15168be2ee971ceef77884ff8791442c9527ee47
irenicaa <root@irenica.pro>
2020-10-31 21:50:53 +0300

    Check the gitlab.SendRequest function

--- cmd/sources-checker/main.go
+++ cmd/sources-checker/main.go
@@ -4,13 +4,20 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net/url"
 
 	"github.com/irenicaa/repos-checker/loader/sources/bitbucket"
 	"github.com/irenicaa/repos-checker/loader/sources/github"
+	"github.com/irenicaa/repos-checker/loader/sources/gitlab"
 )
 
+type user struct {
+	Name  string
+	State string
+}
+
 func main() {
-	source := flag.String("source", "bitbucket", "")
+	source := flag.String("source", "gitlab", "")
 	flag.Parse()
 
 	switch *source {
@@ -30,6 +37,17 @@ func main() {
 		}
 
 		fmt.Printf("%s %+v\n", source.Name(), reposStates)
+	case "gitlab":
+		parameters := url.Values{}
+		parameters.Add("username", "dzaporozhets")
+
+		var users []user
+		err := gitlab.SendRequest("/users", parameters, &users)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("%+v\n", users)
 	default:
 		log.Fatal("unknown source")
 	}
