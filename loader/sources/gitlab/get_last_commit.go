e60252d4f6a286448082b0b40ccab205967892a6
irenicaa <root@irenica.pro>
2020-10-31 22:31:34 +0300

    Add the gitlab.SendRequest function

---
+++ loader/sources/gitlab/get_last_commit.go
@@ -0,0 +1,34 @@
+package gitlab
+
+import (
+	"errors"
+	"fmt"
+	"net/url"
+	"path"
+
+	"github.com/irenicaa/repos-checker/models"
+)
+
+type commit struct {
+	SHA string `json:"id"`
+}
+
+// GetLastCommit ...
+func GetLastCommit(repoPath string) (models.RepoState, error) {
+	parameters := url.Values{}
+	parameters.Add("per_page", "1")
+
+	var commits []commit
+	escapedRepoPath := url.PathEscape(repoPath)
+	endpoint := fmt.Sprintf("/projects/%s/repository/commits", escapedRepoPath)
+	if err := SendRequest(endpoint, parameters, &commits); err != nil {
+		return models.RepoState{}, err
+	}
+	if len(commits) == 0 {
+		return models.RepoState{}, errors.New("no commits")
+	}
+
+	_, repo := path.Split(repoPath)
+	repoState := models.RepoState{Name: repo, LastCommit: commits[0].SHA}
+	return repoState, nil
+}
