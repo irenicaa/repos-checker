18afbb08dbcb2639948eea1613de34304a0affa8
irenicaa <root@irenica.pro>
2020-11-02 19:03:15 +0300

    Add the gitlab.MakeAuthHeader function

--- loader/sources/gitlab/send_request.go
+++ loader/sources/gitlab/send_request.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"os"
 )
 
 // MakeURL ...
@@ -15,6 +16,16 @@ func MakeURL(endpoint string, parameters url.Values) string {
 		endpoint,
 		parameters.Encode(),
 	)
+}
+
+// MakeAuthHeader ...
+func MakeAuthHeader() (string, bool) {
+	token := os.Getenv("GITLAB_TOKEN")
+	if token == "" {
+		return "", false
+	}
+
+	return "Bearer " + token, true
 }
 
 // SendRequest ...
@@ -27,6 +38,11 @@ func SendRequest(
 	request, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
 		return fmt.Errorf("unable to create the request: %v", err)
+	}
+
+	authHeader, ok := MakeAuthHeader()
+	if ok {
+		request.Header.Add("Authorization", authHeader)
 	}
 
 	response, err := http.DefaultClient.Do(request)
