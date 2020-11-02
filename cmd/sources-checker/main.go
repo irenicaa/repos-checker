1708e0fde0ef54ff90ef212f1720be07d3fb4346
irenicaa <root@irenica.pro>
2020-11-02 17:55:03 +0300

    Check the gitlab.Source structure

--- cmd/sources-checker/main.go
+++ cmd/sources-checker/main.go
@@ -12,7 +12,6 @@ import (
 
 func main() {
 	source := flag.String("source", "gitlab", "")
-	pageSize := flag.Int("pageSize", 100, "")
 	flag.Parse()
 
 	switch *source {
@@ -33,12 +32,13 @@ func main() {
 
 		fmt.Printf("%s %+v\n", source.Name(), reposStates)
 	case "gitlab":
-		reposPaths, err := gitlab.GetRepos("dzaporozhets", *pageSize)
+		source := gitlab.Source{Owner: "dzaporozhets"}
+		reposStates, err := source.LoadRepos()
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		fmt.Printf("%+v\n", reposPaths)
+		fmt.Printf("%s %+v\n", source.Name(), reposStates)
 	default:
 		log.Fatal("unknown source")
 	}
