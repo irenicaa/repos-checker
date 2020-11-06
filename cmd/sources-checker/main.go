d43a2099368f86dc46884ee021bca335af993709
irenicaa <root@irenica.pro>
2020-11-06 19:16:36 +0300

    Check the external.Source structure

--- cmd/sources-checker/main.go
+++ cmd/sources-checker/main.go
@@ -7,13 +7,14 @@ import (
 	"os"
 
 	"github.com/irenicaa/repos-checker/loader/sources/bitbucket"
+	"github.com/irenicaa/repos-checker/loader/sources/external"
 	filesystem "github.com/irenicaa/repos-checker/loader/sources/file-system"
 	"github.com/irenicaa/repos-checker/loader/sources/github"
 	"github.com/irenicaa/repos-checker/loader/sources/gitlab"
 )
 
 func main() {
-	source := flag.String("source", "file-system", "")
+	source := flag.String("source", "external", "")
 	flag.Parse()
 
 	logger := log.New(os.Stderr, "", log.LstdFlags)
@@ -44,6 +45,18 @@ func main() {
 		fmt.Printf("%s %+v\n", source.Name(), reposStates)
 	case "file-system":
 		source := filesystem.Source{BasePath: ".."}
+		reposStates, err := source.LoadRepos()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("%s %+v\n", source.Name(), reposStates)
+	case "external":
+		source := external.Source{
+			AdditionalName: "test",
+			Command:        "./tools/test_tool.bash",
+			Arguments:      []string{".."},
+		}
 		reposStates, err := source.LoadRepos()
 		if err != nil {
 			log.Fatal(err)
