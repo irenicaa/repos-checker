cd5230cb5f3c4fc4fc87bdf1addf7d427a291ae1
irenicaa <root@irenica.pro>
2020-11-12 02:55:08 +0300

    Fix the filesystem.GetLastCommit function

--- loader/sources/file-system/get_last_commit.go
+++ loader/sources/file-system/get_last_commit.go
@@ -1,27 +1,41 @@
 package filesystem
 
 import (
+	"bytes"
 	"fmt"
 	"path/filepath"
 	"strings"
 
+	sourceutils "github.com/irenicaa/repos-checker/loader/sources/source-utils"
 	"github.com/irenicaa/repos-checker/models"
 	systemutils "github.com/irenicaa/repos-checker/system-utils"
 )
 
 // GetLastCommit ...
 func GetLastCommit(repoPath string) (models.RepoState, error) {
-	commandOutput, err := systemutils.RunCommand(
+	statusOutput, err := systemutils.RunCommand(
+		"git",
+		[]string{"status"},
+		repoPath,
+		map[string]string{"LC_ALL": "en_US"},
+	)
+	if err != nil {
+		return models.RepoState{}, fmt.Errorf("unable to get a git status: %v", err)
+	}
+
+	statusOutput = bytes.ToLower(statusOutput)
+	if bytes.Index(statusOutput, []byte("no commits yet")) != -1 {
+		return models.RepoState{}, sourceutils.ErrNoCommits
+	}
+
+	logOutput, err := systemutils.RunCommand(
 		"git",
 		[]string{"log", "--format=%H", "HEAD~.."},
 		repoPath,
 		nil,
 	)
 	if err != nil {
-		return models.RepoState{}, fmt.Errorf(
-			"an error occurred while running the command: %v",
-			err,
-		)
+		return models.RepoState{}, fmt.Errorf("unable to get a git log: %v", err)
 	}
 
 	absoluteRepoPath, err := filepath.Abs(repoPath)
@@ -35,7 +49,7 @@ func GetLastCommit(repoPath string) (models.RepoState, error) {
 	_, repo := filepath.Split(absoluteRepoPath)
 	repoState := models.RepoState{
 		Name:       repo,
-		LastCommit: strings.TrimSpace(string(commandOutput)),
+		LastCommit: strings.TrimSpace(string(logOutput)),
 	}
 
 	return repoState, nil
