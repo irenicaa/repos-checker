cc5f8deaceaa6220032c1b712eac93a4d91293b1
irenicaa <root@irenica.pro>
2020-11-13 02:53:03 +0300

    Fix a code style of the general code

--- cmd/sources-checker/main.go
+++ cmd/sources-checker/main.go
@@ -11,6 +11,7 @@ import (
 	filesystem "github.com/irenicaa/repos-checker/loader/sources/file-system"
 	"github.com/irenicaa/repos-checker/loader/sources/github"
 	"github.com/irenicaa/repos-checker/loader/sources/gitlab"
+	"github.com/irenicaa/repos-checker/models"
 )
 
 func main() {
@@ -19,6 +20,9 @@ func main() {
 
 	const maxPageSize = 100
 	logger := log.New(os.Stderr, "", log.LstdFlags)
+	var sourceName string
+	var reposStates []models.RepoState
+	var err error
 	switch *source {
 	case "github":
 		source := github.Source{
@@ -26,57 +30,42 @@ func main() {
 			MaxPageSize: maxPageSize,
 			Logger:      logger,
 		}
-		reposStates, err := source.LoadRepos()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Printf("%s %+v\n", source.Name(), reposStates)
+		sourceName = source.Name()
+		reposStates, err = source.LoadRepos()
 	case "bitbucket":
 		source := bitbucket.Source{
 			Workspace:   "MartinFelis",
 			MaxPageSize: maxPageSize,
 			Logger:      logger,
 		}
-		reposStates, err := source.LoadRepos()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Printf("%s %+v\n", source.Name(), reposStates)
+		sourceName = source.Name()
+		reposStates, err = source.LoadRepos()
 	case "gitlab":
 		source := gitlab.Source{
 			Owner:       "dzaporozhets",
 			MaxPageSize: maxPageSize,
 			Logger:      logger,
 		}
-		reposStates, err := source.LoadRepos()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Printf("%s %+v\n", source.Name(), reposStates)
+		sourceName = source.Name()
+		reposStates, err = source.LoadRepos()
 	case "file-system":
 		source := filesystem.Source{BasePath: "..", Logger: logger}
-		reposStates, err := source.LoadRepos()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Printf("%s %+v\n", source.Name(), reposStates)
+		sourceName = source.Name()
+		reposStates, err = source.LoadRepos()
 	case "external":
 		source := external.Source{
 			AdditionalName: "test",
 			Command:        "./tools/test_tool.bash",
 			Arguments:      []string{".."},
 		}
-		reposStates, err := source.LoadRepos()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Printf("%s %+v\n", source.Name(), reposStates)
+		sourceName = source.Name()
+		reposStates, err = source.LoadRepos()
 	default:
 		log.Fatal("unknown source")
 	}
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%s %+v\n", sourceName, reposStates)
 }
