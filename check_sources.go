e99ea842cef333940555267ae714df26144c9b7c
irenicaa <root@irenica.pro>
2020-11-18 02:57:56 +0300

    Add a base of the reposchecker.CheckSources function

---
+++ check_sources.go
@@ -0,0 +1,27 @@
+package reposchecker
+
+import (
+	"github.com/irenicaa/repos-checker/comparer"
+	"github.com/irenicaa/repos-checker/loader"
+	"github.com/irenicaa/repos-checker/models"
+)
+
+// CheckSources ...
+func CheckSources(
+	sources []loader.Source,
+	referenceName string,
+	logger loader.Logger,
+) []comparer.SourceDiff {
+	var reference models.SourceState
+	var rest []models.SourceState
+	sourceStates := loader.LoadSources(sources, logger)
+	for _, sourseState := range sourceStates {
+		if sourseState.Name == referenceName {
+			reference = sourseState
+		} else {
+			rest = append(rest, sourseState)
+		}
+	}
+
+	return comparer.CompareSources(rest, reference)
+}
