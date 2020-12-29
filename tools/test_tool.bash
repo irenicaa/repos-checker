0540aac73b4ec41928e6314642624dc6f4f23888
irenicaa <root@irenica.pro>
2020-12-29 19:04:52 +0300

    Fix the example of an external tool

--- tools/test_tool.bash
+++ tools/test_tool.bash
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-declare basePath=$1
-find $basePath -type d -name ".git" \
-  | while read gitDirectoryPath; do
-    declare repoPath=$(dirname $gitDirectoryPath)
-    pushd $repoPath > /dev/null
+declare basePath="$1"
+find "$basePath" -type d -name ".git" \
+  | while read -r gitDirectoryPath; do
+    declare repoPath="$(dirname "$gitDirectoryPath")"
+    pushd "$repoPath" > /dev/null
 
-    declare name=$(basename $repoPath)
+    declare name="$(basename "$repoPath")"
     declare lastCommit=$(git rev-parse HEAD)
     printf '{"name":"%s","lastCommit":"%s"}\n' $name $lastCommit
 
